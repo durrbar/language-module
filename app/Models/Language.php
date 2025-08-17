b3d15f14649ef84ab35270170ded33a9c3203691
Kid Max <kidmax285@gmail.com>
2025-08-17 13:23:24 +0600

    Adds UUID support to language model

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -2,10 +2,13 @@
 
 namespace Modules\Language\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
+    use HasUuids;
+    
     protected $table = 'languages';
 
     public $guarded = [];
